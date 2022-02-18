eff94aa14ffb777a91a3b7d130d20849558244c5
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-02-18 16:47:14 +0700

    Add required to skill form.

--- 6310742181/resources/views/admin/skills.blade.php
+++ 6310742181/resources/views/admin/skills.blade.php
@@ -39,11 +39,11 @@
             @csrf
             <div class="mb-2">
                 <label htmlFor="formName">Skill Name</label>
-                <input type="text" name="name" id="formName" class="form-control"/>
+                <input type="text" name="name" id="formName" class="form-control" required/>
             </div>
             <div class="mb-2">
                 <label htmlFor="formPercentage">Skill Percentage</label>
-                <input type="number" name="percentage" id="formPercentage" min="0" max="100" step="1" class="form-control"/>
+                <input type="number" name="percentage" id="formPercentage" min="0" max="100" step="1" class="form-control" required/>
             </div>
             <div class="text-end mt-4">
                 <button type="submit" class="btn btn-success">Create new skill</button>
--- 6310742181/resources/views/admin/skills_edit.blade.php
+++ 6310742181/resources/views/admin/skills_edit.blade.php
@@ -9,11 +9,11 @@
             @csrf
             <div class="mb-2">
                 <label htmlFor="formName">Skill Name</label>
-                <input type="text" name="name" id="formName" value="{{ $skills['name'] }}" class="form-control"/>
+                <input type="text" name="name" id="formName" value="{{ $skills['name'] }}" class="form-control" required/>
             </div>
             <div class="mb-2">
                 <label htmlFor="formPercentage">Skill Percentage</label>
-                <input type="number" name="percentage" id="formPercentage" min="0" max="100" step="1" value="{{ $skills['percentage'] }}" class="form-control"/>
+                <input type="number" name="percentage" id="formPercentage" min="0" max="100" step="1" value="{{ $skills['percentage'] }}" class="form-control" required/>
             </div>
             <div class="text-end mt-4">
                 <button type="submit" class="btn btn-success">Save</button>
